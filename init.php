34f5a5c0314c60b0e9ab35e4be8b268e3ff39718
Alexey Berdnikov <aberdnikov@gmail.com>
2016-04-25 12:36:55 +0500

    2.2.3

--- init.php
+++ init.php
@@ -1,3 +1,3 @@
 <?php
 \Larakit\StaticFiles\Manager::package('larakit/sf-jquery')
-                            ->js('//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js');
+                            ->js('//cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min.js');
