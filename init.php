8e02a3076b2bf59a513929cbc4dad74dbc3fb9d5
Alexey Berdnikov <aberdnikov@gmail.com>
2016-07-11 21:44:56 +0500

    fixed

--- init.php
+++ init.php
@@ -1,3 +1,4 @@
 <?php
 \Larakit\StaticFiles\Manager::package('larakit/sf-jquery')
-                            ->js('//cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min.js');
+    ->setSourceDir('public')
+    ->jsPackage('jquery.min.js');
